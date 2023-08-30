ccd0fdf4bd67258d0e05868484480660de2d8d74
Jugen <jugendrabhati658@gmail.com>
2023-08-30 14:06:47 +0530

    ...

--- app/src/main/java/com/jdpublication/news/utils/Config.java
+++ app/src/main/java/com/jdpublication/news/utils/Config.java
@@ -3,5 +3,5 @@ package com.jdpublication.news.utils;
 public class Config {
 
     public static String BASE_URL = "https://newsapi.org/v2/top-headlines";
-    public static String API_KEY = "fa9119be0f934322b33c5366b42c1471";
+    public static String API_KEY = "5e145a9a7a50486ead37f7cfa201feb2";
 }
